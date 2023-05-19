98c74e3fd3d32badce0982cb21123b3a1017fc85
pramodkhavare <114791765+pramodkhavare@users.noreply.github.com>
2023-05-19 14:27:10 +0530

    Add files via upload

---
+++ rough.py
@@ -0,0 +1,29 @@
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+ 
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+print()
+
+import requests
+
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+print(get_location())
